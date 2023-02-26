db5eeaf6bdebf72aef06a91280f44e336cf50a46
CelesTech03 <celeste.urena@stu.bmcc.cuny.edu>
2023-02-25 21:30:33 -0500

    Added Navbar component & refactored code

--- src/App.js
+++ src/App.js
@@ -3,54 +3,22 @@ import Restaurants from "./components/Restaurants";
 import Homepage from "./components/Homepage";
 import Attractions from "./components/Attractions";
 import Lodging from "./components/Lodging";
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Springfield</h1>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Homepage />} />
-          <Route path="restaurants" element={<Restaurants />} />
-          <Route path="attractions" element={<Attractions />} />
-          <Route path="lodging" element={<Lodging />} />
-        </Route>
-      </Routes>
-    </div>
-  );
-}
-
-function Layout() {
-  return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/restaurants">
-              Restaurants
-            </Link>
-          </li>
-          <li>
-            <Link to="/attractions">
-              Attractions
-            </Link>
-          </li>
-          <li>
-            <Link to="/lodging">
-              Lodging
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <hr />
-      <Outlet />
-    </div>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="/restaurants" element={<Restaurants />} />
+            <Route path="/attractions" element={<Attractions />} />
+            <Route path="/lodging" element={<Lodging />} />
+          </Routes>
+        </div>
+      </div>
   );
 }
 
